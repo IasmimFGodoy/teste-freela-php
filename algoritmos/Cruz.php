ecf37c25980f98b1debf62663f537d35cbb0ff65
IasmimFGodoy <iasmimgodoy@outlook.com>
2023-03-31 13:18:27 -0300

    first commit

---
+++ algoritmos/Cruz.php
@@ -0,0 +1,20 @@
+<?php
+$cruz = '.......';
+$cruz[3] = '*';
+echo $cruz;
+echo "\n";
+$cruz[1] = '*';
+$cruz[2] = '*';
+$cruz[3] = '*';
+$cruz[4] = '*';
+$cruz[5] = '*';
+echo $cruz;
+$cruz = '.......';
+$cruz[3] = '*';
+echo "\n";
+echo $cruz;
+echo "\n";
+echo $cruz;
+echo "\n";
+echo $cruz;
+?>
